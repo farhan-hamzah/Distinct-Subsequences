d2ea498f1f269671e78da6d808035d711b7b0924
Farhan Hamzah <177802321+farhan-hamzah@users.noreply.github.com>
2025-08-04 21:28:23 +0700

    Add files via upload

---
+++ leetCode.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+        if n > m:
+            return 0
+        
+        # dp[i][j] = # of ways to form t[:j] from s[:i]
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        # Empty t can be formed from any s[:i] in exactly 1 way (by deleting all chars)
+        for i in range(m + 1):
+            dp[i][0] = 1
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+
+        return dp[m][n]
